647a7ceb6b210dd566801c9d92f06010d60677da
Md. Manzum Rahman <manzumrahman@gmail.com>
2021-09-23 23:48:15 +0600

    added preloader

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const toggle_button = document.getElementsByClassName('toggle-button')[0]
-const navbar_nav = document.getElementsByClassName('navbar-nav')[0]
-const navbar = document.getElementsByClassName('navbar')[0]
-
 toggle_button.addEventListener('click', () =>{
+    const navbar_nav = document.getElementsByClassName('navbar-nav')[0]
+    const navbar = document.getElementsByClassName('navbar')[0]
     navbar_nav.classList.toggle('active')
     navbar.classList.toggle('active')
 })
@@ -13,3 +12,11 @@ window.addEventListener('scroll', ()=>{
     gotop_btn.classList.toggle('show', window.scrollY > 0)
 
 })
+window.addEventListener('load', ()=>{
+    const intro = document.getElementsByClassName('intro-bg')[0]
+    const preloader = document.getElementById('preloader')
+    intro.style.display = 'block'
+    preloader.style.display = 'none'
+
+})
+
